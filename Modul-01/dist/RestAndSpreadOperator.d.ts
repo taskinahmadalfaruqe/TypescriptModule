2a7ad9fd1afa8b93e984683d9f97253d61ef05f4
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2026-05-16 23:19:39 +0800

    [ObjectSpreadAndRest Operator]: Add Object Spread Operator And Ads Function Rest Operator With ForEach Loop.

---
+++ Modul-01/dist/RestAndSpreadOperator.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=RestAndSpreadOperator.d.ts.map
--- Modul-01/dist/RestAndSpreadOperator.js
+++ Modul-01/dist/RestAndSpreadOperator.js
@@ -1,30 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Rest And Spread Operator
 // Spread Operator
 const arr1 = [1, 2, 3];
 const arr2 = [...arr1, 4, 5];
 console.log(arr2); // Output: [1, 2, 3, 4, 5]
-
 // sperade in objects
 const user = {
     name: "Taskin Ahmad Al Faruqe",
     age: 25,
-}
+};
 const updateUserInfo = {
     Address: "Dhaka, Bangladesh",
-}
+};
 const updateUserInformations = {
     ...user,
     ...updateUserInfo
 };
-console.log(updateUserInformations)
-
+console.log(updateUserInformations);
 // Rest Operator
 const myFriends = ["Alice", "Bob", "Charlie", "David"];
-const sendInvita = (...friends: string[]) => {
-    friends.forEach((friend: string) => {
+const sendInvita = (...friends) => {
+    friends.forEach((friend) => {
         console.log(`You Are Invited: ${friend}`);
     });
-
-}
-
-sendInvita("t","a","s"); // Output: You Are Invited: Alice,Bob,Charlie,David
+};
+sendInvita("t", "a", "s"); // Output: You Are Invited: Alice,Bob,Charlie,David
+//# sourceMappingURL=RestAndSpreadOperator.js.map
