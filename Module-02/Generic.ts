efe0769b57b4730f612f32f35df97e95307c133f
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2026-05-25 03:49:35 +0800

    [Array Of Object]: Generic With Array of Object.

--- Module-02/Generic.ts
+++ Module-02/Generic.ts
@@ -7,3 +7,20 @@ const userArray: genericArray<string> = ["John", "Doe", "Jane"];
 type genericTuple<X, Y> = [X, Y];
 const TupleAdd: genericTuple<number, number> = [5, 10];
 const TUPLE: genericTuple<number, string> = [2, 'hELLO']
+
+type genericObject = {
+    name: string;
+    age: number;
+}
+
+type genericArrayOfObject<T> = Array<T>;
+const genericWithArrayOfObject: genericArrayOfObject<{ name: string, age: number }> = [
+    {
+        name: "Taskin",
+        age: 25,
+    },
+    {
+        name: "John",
+        age: 30,
+    },
+]
